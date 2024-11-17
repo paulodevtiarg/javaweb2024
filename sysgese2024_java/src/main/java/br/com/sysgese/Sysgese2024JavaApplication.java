2836415f188c4765ee413047dd06d847e7ebb0f9
paulohorizonti <pnagi2012@gmail.com>
2024-11-17 17:37:38 -0300

    first commit

---
+++ sysgese2024_java/src/main/java/br/com/sysgese/Sysgese2024JavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.sysgese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sysgese2024JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sysgese2024JavaApplication.class, args);
+	}
+
+}
---
+++ sysgese2024_java/src/test/java/br/com/sysgese/Sysgese2024JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sysgese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sysgese2024JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
